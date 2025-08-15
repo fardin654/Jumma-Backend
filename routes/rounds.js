993d95711b0f908389ee4f8cba972bb57f585462
fardin654 <fardinmd654@gmail.com>
2025-08-16 01:29:44 +0530

    Still Fixing CORS Issue

--- routes/rounds.js
+++ routes/rounds.js
@@ -233,15 +233,4 @@ router.post('/:roundId/expenses', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
-  try {
-    const member = await Member.findById(req.params.id);
-    if (!member) {
-      return res.status(404).json({ message: 'Member not found' });
-    }
-    res.json(member);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
 module.exports = router;
